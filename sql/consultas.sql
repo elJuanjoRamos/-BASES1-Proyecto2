e95c533ee4f6e8a444392b21c702c9a08181d800
elJuanjoRamos <jjramca1010@gmail.com>
2021-04-20 20:57:00 -0600

    [ADDED] Consulta 1

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -1,2 +1,9 @@
 USE Proyecto2;
+/*CONSULTA 1*/
+SELECT profesional, P.nombre AS NOMBRE_PROFESIONAL, COUNT(*) as inventos_asignados FROM Asignacion_Invento AI
+JOIN Profesional P on P.id = AI.profesional
+GROUP BY profesional
+ORDER BY inventos_asignados desc
+
+
 
